880e8363bdbdeacb19a5fafb8370a5c30e5cd7fd
Gerardo Rodriguez <garodriguez@loto.hn>
2026-03-24 10:31:27 -0600

    fixes

--- apps/ronda/models.py
+++ apps/ronda/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from apps.usuario.models import tbl_usuario 
 
 class tbl_rondas(models.Model):
+    pregunta_inicial = models.ForeignKey(
+        'pregunta.tbl_preguntas',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
     id_ronda = models.AutoField(primary_key=True)
     id_usuarios = models.ForeignKey(tbl_usuario, on_delete=models.CASCADE, related_name='rondas')
     puntaje_total = models.FloatField(default=0.0, blank=False)
